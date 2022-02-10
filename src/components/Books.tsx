6e2f9bd086d2b51d1cb8310d08cd0c9d294dd845
Sudhashree6699 <sudharaj681@gmail.com>
2022-02-10 17:11:56 +0530

    commit

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,36 +1,36 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar , IonList, IonItem, IonLabel, IonAvatar,IonIcon,IonFabButton,IonFab, IonSearchbar, IonThumbnail } from '@ionic/react';
-import { add, book, pencil, pencilSharp, trash, trashBin,} from 'ionicons/icons';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonAvatar, IonIcon, IonFabButton, IonFab, IonSearchbar, IonThumbnail } from '@ionic/react';
+import { add, book, pencil, pencilSharp, trash, trashBin, } from 'ionicons/icons';
 import { useState } from 'react';
 import React from 'react';
-import { getBook,deleteBook } from '../services/BooksRestAPI';
+import { getBook, deleteBook } from '../services/BooksRestAPI';
 import { useHistory } from 'react-router';
 
 interface myProps {
-    random: number;
- }
-interface myCustomer {
-    id: 0,
-    title: "",
-    author: "",
-    publisher: "",
-    isbn: "",
-    year: '',
-    cover: ''
+  random: number;
+}
+interface myBook {
+  id: 0,
+  title: "",
+  author: "",
+  publisher: "",
+  isbn: "",
+  year: '',
+  cover: ''
 }
 
 const BookList = () => {
   const history = useHistory();
+  let books: myBook[] = [];
   const [searchText, setSearchText] = useState('');
-  const [bookList,setBooks] = useState([
-  ])
+  const [bookList, setBooks] = useState(books)
   const loadBooks = async () => {
     let tempData = await getBook();
     setBooks(tempData)
     console.log(tempData)
   }
-  const deleteBooks = async (bookId:any) => {
-        await deleteBook(bookId);
-        loadBooks()
+  const deleteBooks = async (bookId: any) => {
+    await deleteBook(bookId);
+    loadBooks()
   }
 
   React.useEffect(() => {        //executed whenever component is mounted
@@ -38,9 +38,9 @@ const BookList = () => {
       loadBooks();
     });
     loadBooks();
-    return(unlisten);
-  },[]);
- 
+    return (unlisten);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -53,39 +53,38 @@ const BookList = () => {
       </IonHeader>
 
       <IonContent fullscreen>
-      <IonSearchbar onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus"></IonSearchbar>
-       <IonList>
-        {bookList.map((data:any)=>{
-          return(
-            <IonItem key={data.id}>
-            <IonThumbnail slot="start">
-              <img src= {data.cover} />
-            </IonThumbnail>
-            <IonLabel>
-              <h2>ID: {data.id}</h2>
-              <h3>{data.title}</h3>
-              <h3>{data.author}</h3>
-              <h3>{data.publisher}</h3>
-              <h3>{data.year}</h3>
-            </IonLabel>
-              <IonIcon 
-              onClick={()=>{history.push('/EditBook/'+data.id)}}
-              size={'10'} icon={pencil} />
-              <IonIcon onClick={()=>
-              {deleteBooks(data.id)}} 
-              size={'10'} icon={trash} />      
-          </IonItem>
-          )
-        })}
-        </IonList>        
-          <IonFab vertical="bottom" horizontal="end"slot="fixed">   
+        <IonSearchbar onIonChange={e => setSearchText(e.detail.value!)} showCancelButton="focus"></IonSearchbar>
+        <IonList>
+          {bookList.map((data: any) => {
+            return (
+              <IonItem key={data.id}>
+                <IonThumbnail slot="start">
+                  <img src={data.cover} />
+                </IonThumbnail>
+                <IonLabel>
+                  <h2>ID: {data.id}</h2>
+                  <h3>{data.title}</h3>
+                  <h3>{data.author}</h3>
+                  <h3>{data.publisher}</h3>
+                  <h3>{data.year}</h3>
+                </IonLabel>
+                <IonIcon
+                  onClick={() => { history.push('/EditBook/' + data.id) }}
+                  size={'10'} icon={pencil} />
+                <IonIcon onClick={() => { deleteBooks(data.id) }}
+                  size={'10'} icon={trash} />
+              </IonItem>
+            )
+          })}
+        </IonList>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
           <a href="/AddBook" >
-          <IonFabButton>
-              <IonIcon icon={add}/>
+            <IonFabButton>
+              <IonIcon icon={add} />
             </IonFabButton>
-            </a>
-          </IonFab>
-        </IonContent>
+          </a>
+        </IonFab>
+      </IonContent>
     </IonPage>
   );
 };
